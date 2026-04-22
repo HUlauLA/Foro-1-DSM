f4cee4b694050857f9ce44bc30db5983dede6539
YenyRM <rossrecinos05@gmail.com>
2026-04-21 19:10:22 -0600

    Agrego pantalla de Bienvenida

--- app/src/main/java/com/example/notasbienvenida/PantallaBienvenida.kt
+++ app/src/main/java/com/example/notasbienvenida/PantallaBienvenida.kt
@@ -8,7 +8,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
+
 
 @Composable
 fun PantallaBienvenida(
