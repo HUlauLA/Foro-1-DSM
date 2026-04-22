40d641c3191d9cd728521ebc8afced93984faef1
YenyRM <rossrecinos05@gmail.com>
2026-04-21 19:08:35 -0600

    pantalla de Bienvenida

---
+++ app/src/main/java/com/example/notasbienvenida/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.notasbienvenida
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            PantallaBienvenida(
+                nombreUsuario = "Usuario",
+                onContinuar = {
+                    // Aquí después puedes hacer navegación
+                }
+            )
+        }
+    }
+}
---
+++ app/src/main/java/com/example/notasbienvenida/PantallaBienvenida.kt
@@ -0,0 +1,41 @@
+package com.example.notasbienvenida
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun PantallaBienvenida(
+    nombreUsuario: String,
+    onContinuar: () -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Text(
+            text = "Bienvenida, $nombreUsuario 👋",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Button(
+            onClick = { onContinuar() },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "Ingresar notas")
+        }
+    }
+}
